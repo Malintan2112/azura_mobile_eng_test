b389c8c3ccf0ad20653bbc651b5985e1f9f19051
Mojave Maniac <malintanhernawanputra.net@gmail.com>
2021-09-06 23:53:34 +0700

    add fastlane

--- android/app/src/main/java/com/azura/test/MainApplication.java
+++ android/app/src/main/java/com/azura/test/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
